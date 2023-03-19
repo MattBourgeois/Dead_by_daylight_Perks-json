0489a16c17a9400cfaae0e64848b2e713635c0f2
MattBourgeois <Bourgeoismatthew6@gmail.com>
2023-03-19 12:06:40 -0400

    starting to added survivors

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import json
-# Keys: Perks, 
-with open("api.json", "r") as f:
-	data = json.load(f)
+import random
+# Keys: Number ,Name, Perks, Perk Description, Add ons
 
-for i in data:
-	print(data['Name'])
+with open("Survivor.json", "r") as d:
+	data = json.load(d)
 
-# print(data["Name"]["Perks"])
-# print(data["20"]["Perk Descriptions"][0])
+print(data["Players"]["Killers"]['5']["Perks"])
+print(data["Players"]["Killers"]['5']['Perks'][0])  #Gets the Perk
+print(data["Players"]["Killers"]["5"]["Perk Description"][0]) #Gets the perk Description
+
+
+print(data["Players"]["Killers"]["0"]["Name"])
+print(data["Players"]["Survivors"]["0"]["Name"])
+
+
+
+# Creates new json file 
+# with open("data2.json", "w") as f: 
+# 	json.dump(data, f)
