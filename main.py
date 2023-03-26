36d5118f111af0f93c3188df43b1155c4fc07d12
MattBourgeois <Bourgeoismatthew6@gmail.com>
2023-03-26 11:15:54 -0400

    break

--- main.py
+++ main.py
@@ -5,18 +5,18 @@ import random
 with open("data.json", "r") as d:
 	data = json.load(d)
 
-print(data["Players"]["Killers"]['5']["Perks"])
-print(data["Players"]["Killers"]['5']['Perks'][0])  #Gets the Perk
-print(data["Players"]["Killers"]["5"]["Perk_Description"][0]) #Gets the perk Description
+# print(data["Players"]["Killers"]['5']["Perks"])
+# print(data["Players"]["Killers"]['5']['Perks'][0])  #Gets the Perk
+# print(data["Players"]["Killers"]["5"]["Perk_Description"][0]) #Gets the perk Description
 
 
-print(data["Players"]["Killers"]["0"]["Name"])
-print(data["Players"]["Survivors"]["0"]["Name"])
-for x in range(0, len(data), 1):
-	print(data["Players"]["Killers"])
+# print(data["Players"]["Killers"]["0"]["Name"])
+# print(data["Players"]["Survivors"]["0"]["Name"])
+# for x in range(0, len(data)):
+# 	print(data["Players"]["Killers"]["0"]["Name"])
 
 
 
 # Creates new json file 
-# with open("data2.json", "w") as f: 
-# 	json.dump(data, f)
+with open("data2.json", "w") as f: 
+	json.dump(data, f)
