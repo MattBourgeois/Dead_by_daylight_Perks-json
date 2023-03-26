78732397053834f70ccfe4b039b41071c4d420fb
MattBourgeois <Bourgeoismatthew6@gmail.com>
2023-03-26 13:12:35 -0400

    removed 'Players', unnecisary

--- test.py
+++ test.py
@@ -7,6 +7,6 @@ import json
 with open("data.json", "r") as d:
 	data = json.load(d)
 
-print(data["Players"]["Killers"]["21"]["Perks"][0])
-print(data["Players"]["Killers"]["21"]["Perk_Description"][0])
-print(data["Players"]["Survivors"]["1"]["Perks"])
+print(data["Killers"]["21"]["Perks"][0])
+print(data["Killers"]["21"]["Perk_Description"][0])
+print(data["Survivors"]["1"]["Perks"])
