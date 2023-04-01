55d0db0e91f2799c05ebb92c48b533d4cd3efe16
MattBourgeois <Bourgeoismatthew6@gmail.com>
2023-03-31 21:00:32 -0400

    fixed metho to show

--- data.py
+++ data.py
@@ -1,4 +1,5 @@
 import json
+import random
 # Keys: Number ,Name, Perks, Perk_Description, Add_ons
 with open("data.json", "r") as d:
 	data = json.load(d)
@@ -7,15 +8,15 @@ with open("data.json", "r") as d:
 # Survivor_Perks = data["Players"]["Survivors"]["1"]["Perks"]
 
 class Killer:
-	def __init__(self, Name, Perks, Perk_description, Add_ons):
+	def __init__(self, Name, Perks, Perk_Description, Add_ons):
 		self.Name = Name
 		self.Perks = Perks
-		self.Perk_description = Perk_description
+		self.Perk_description = Perk_Description
 		self.Add_ons = Add_ons
 
-	def GetKillerPerks(random ,num):
-
-		print( data["Killers"][random]["Perks"][num])
+	def GetKillerPerks(killer_num ,perk_num):
+		print( data["Killers"][killer_num]["Perks"][perk_num])
+		print(data["Killers"][killer_num]["Perk_Description"][perk_num])
 
 	def create(self):
 		pass
@@ -24,8 +25,9 @@ class Killer:
 	# 	print(f"This is {self.Name} who has {self.Perks(data)} which means {self.Perk_description[x]}")
 
 # Killer.GetKillerPerks(self, Name, Perks, Perk_Description, Add_ons)
-
+x = random.randint(1, 30)
+y = random.randint(0,2)
 # print(Killer_Perk)
 # print(Killer.GetKillerPerks(self, Name, Perks, Perk_Description, Add_ons))
 
-Killer.GetKillerPerks("1", 0)
+Killer.GetKillerPerks(f"{x}", 0)
