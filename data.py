bcf4e82d4e22d5162f7c0a8ce5c433f5d4928061
MattBourgeois <Bourgeoismatthew6@gmail.com>
2023-04-02 08:16:06 -0400

    for loop for 4 random killer perks

--- data.py
+++ data.py
@@ -18,14 +18,30 @@ class Killer:
 		print( data["Killers"][killer_num]["Perks"][perk_num])
 		print(data["Killers"][killer_num]["Perk_Description"][perk_num])
 
+class Survivor:
+	def __init__(self, Name, Perks, Perk_Description):
+		self.Name = Name
+		self.Perks = Perks
+		self.Perk_description = Perk_Description
+
+	def GetSuvivorPerks(Sur_perks, perk_num):
+		print(data["Survivors"][Sur_perks]["Perks"][perk_num])
+		print(data["Survivors"][Sur_perks]["Perk_Description"][perk_num])
+
 	def create(self):
 		pass
 
 	# def print(self):
 	# 	print(f"This is {self.Name} who has {self.Perks(data)} which means {self.Perk_description[x]}")
 
-x = random.randint(1, 30)
-y = random.randint(0,2)
+# x = random.randint(0, 30) #Killer numbers
 
+i = random.randint(0, 36) #Survivor numbers
 
-Killer.GetKillerPerks(f"{x}", y)
+# Survivor.GetSuvivorPerks(f"{i}", 0)
+# Killer.GetKillerPerks(f"{x}", y)
+
+for a in range(0,4,1):
+	x = random.randint(0, 30)
+	Killer.GetKillerPerks(f"{x}", 0)
+	
